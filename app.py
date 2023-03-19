c34e79db0313fd1ddd16d1e03048b305d32e45b6
sibi_s_ <ssaravanan339@student.fuhsd.org>
2023-03-18 18:49:52 -0700

    app updates

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 #Import PySimpleGUI
-import PySimpleGUI as sg
- 
-#Draw the button
-layout = [[sg.Button('Hello, New Stack', size=(30,4))]]
- 
-#Draw the window
-window = sg.Window('GUI SAMPLE', layout, size=(1000,700))
- 
-#Define what happens when the button is clicked
-event, values = window.read()
+import PySimpleGUI as psg
+
+def main():
+
+	layout = [
+	   [psg.Text('Select a file',font=('Arial Bold', 20), expand_x=True, justification='center')],
+	   [psg.Input(enable_events=True, key='-IN-',font=('Arial Bold', 12),expand_x=True),psg.FileBrowse()],
+	   [psg.Button('Submit')]
+	]
+	window = psg.Window('FileChooser Demo', layout,
+	size=(1000,700))
+	while True:
+	   event, values = window.read()
+	   if event == psg.WIN_CLOSED or event == 'Exit':
+	      break
+	window.close()
+
+def open_window():
+    layout = [[sg.Text("New Window", key="new")]]
+    window = sg.Window("Second Window", layout, modal=True)
+    choice = None
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+    window.close()
+
+if __name__ == "__main__":
+    main()
